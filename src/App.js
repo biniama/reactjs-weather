60fc487d85e6590b26cff6f78083da6c95d751e6
Biniam Asnake <biniam.asnake@kaufda.de>
2019-01-15 11:24:20 +0100

    Mexico City

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "bootswatch/dist/journal/bootstrap.css";
 import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 
 const PLACES = [
-  { name: "Palo Alto", zip: "94303" },
+  { name: "Mexico City", zip: "94303" },
   { name: "San Jose", zip: "94088" },
   { name: "Santa Cruz", zip: "95062" },
   { name: "Honolulu", zip: "96803" }
